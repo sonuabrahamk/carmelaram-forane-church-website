01d113541f3a7288292c5d4a91c38918b240011f
Sonu Abraham <sonu.abrahamk@gmail.com>
2023-06-24 11:26:40 +0530

    Initial Demo website content

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,20 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgbModule, NgbDropdownModule } from '@ng-bootstrap/ng-bootstrap';
 import { HomeComponent } from './pages/home/home.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { LitergyComponent } from './components/litergy/litergy.component';
+import { ScrollerComponent } from './components/scroller/scroller.component';
+import { CarouselComponent } from './components/home-components/carousel/carousel.component';
+import { UpcomingEventsComponent } from './components/home-components/upcoming-events/upcoming-events.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    NavbarComponent,
+    LitergyComponent,
+    ScrollerComponent,
+    CarouselComponent,
+    UpcomingEventsComponent,
+    FooterComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    NgbModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, NgbModule, NgbDropdownModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/components/litergy/litergy.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-litergy',
+  templateUrl: './litergy.component.html',
+  styleUrls: ['./litergy.component.css']
+})
+export class LitergyComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
