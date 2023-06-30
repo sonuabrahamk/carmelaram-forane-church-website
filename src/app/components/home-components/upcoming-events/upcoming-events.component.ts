2120b9d59613d183a4c9122838c0929b68305728
Sonu Abraham <sonu.abrahamk@gmail.com>
2023-06-30 15:46:20 +0530

    Enquiry and Upcoming events module

--- src/app/components/home-components/upcoming-events/upcoming-events.component.ts
+++ src/app/components/home-components/upcoming-events/upcoming-events.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { eventsData } from '../../constants/events-data';
+import { OwlOptions } from 'ngx-owl-carousel-o';
 
 @Component({
   selector: 'app-upcoming-events',
@@ -12,4 +13,24 @@ export class UpcomingEventsComponent implements OnInit {
   ngOnInit(): void {}
 
   eventsData = eventsData;
+
+  customOptions: OwlOptions = {
+    loop: true,
+    dots: false,
+    navSpeed: 200,
+    autoplay: true,
+    pullDrag: true,
+    mouseDrag: true,
+    touchDrag: true,
+    nav: true,
+    navText: [
+      "<i class='fa-solid fa-backward'></i>",
+      "<i class='fa-solid fa-forward'></i>",
+    ],
+    responsive: {
+      0: {
+        items: 1,
+      },
+    },
+  };
 }
