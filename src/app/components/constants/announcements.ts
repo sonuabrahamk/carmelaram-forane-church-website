59f147f8d537601c36b767c25b7e3c47dfef1e59
Sonu Abraham <sonu.abrahamk@gmail.com>
2023-08-05 06:18:25 +0530

    30th July commit

--- src/app/components/constants/announcements.ts
+++ src/app/components/constants/announcements.ts
@@ -1,7 +1,7 @@
 export const announcements = [
-  'General Body meeting of Mathruvedhi to be held on Sunday, 30th July after 9am Mass',
-  'General Body meeting of Mount Carmel Youth to be held on Sunday, 30th July after 9am Mass',
-  'St. Vincent De Paul Association - Carmelaram Unit has received International Aggregation',
+  'Blood Donation Camp and Free Heart Checkup to be held on Sunday, 6th August , from 8:30am to 1pm at Avila Bhavan.',
+  'Aradhana to be held on Sunday, 6th August at 8.30am led by Mathruvedhi',
+  'St. Vincent De Paul Association - Carmelaram Unit has received International Accredetation.',
   'Required: Parish Office Assistant, Contact Parish Office',
   'Swaralaya Music Academy classes are starting on 01 August, Contact Fr. Biju (8762942340)',
 ];
--- src/app/components/constants/events-data.ts
+++ src/app/components/constants/events-data.ts
@@ -3,32 +3,10 @@ import { IEvents } from '../interfaces/IEvents';
 export const eventsData: IEvents[] = [
   {
     id: 1,
-    date: new Date('2023-07-23'),
-    organised_by: 'Mount Carmel Youth',
-    title: 'Youth Inauguration',
-    content: 'Installation of New MCY Office Bearers for 2023-24',
-  },
-  {
-    id: 2,
-    date: new Date('2023-07-23'),
-    organised_by: 'Catechism',
-    title: 'Bible Kalotsavam - Parish Prelim',
+    date: new Date('2023-08-24'),
+    organised_by: 'Parish',
+    title: 'Retreat by Shalom Team',
     content:
-      'Events - Essay Writing | Story Writing | Poetry Writing | Poster Colouring | Pencil Sketching',
-  },
-  {
-    id: 3,
-    date: new Date('2023-07-29'),
-    organised_by: 'Catechism',
-    title: 'Bible Kalotsavam - Parish Prelims',
-    content:
-      'Events - Bible Reading(Mal) | Bible Story Telling(Mal) | Solo Song | Solo Dance | Elocution-English',
-  },
-  {
-    id: 4,
-    date: new Date('2023-07-30'),
-    organised_by: 'Catechism',
-    title: 'Bible Kalotsavam Parish Prelims',
-    content: 'Events - Elocution(Mal) | Extempore MonoAct | Psalm Reciting',
+      'Retreat from Thursday to Saturday (24th to 26th August) – 5pm to 9pm in ordinance with Mother Mary Feast',
   },
 ];
