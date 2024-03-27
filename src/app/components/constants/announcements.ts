0650727af6291e97ccde8af40ddf3e5cc6c94c7b
Sonu Abraham <sonu.abrahamk@gmail.com>
2024-03-27 08:13:34 +0530

    recent changes

--- src/app/components/constants/announcements.ts
+++ src/app/components/constants/announcements.ts
@@ -1,4 +1,1 @@
-export const announcements = [
-  "St. Joseph's Feast Ootu Nercha to be held on Sunday, 17th March",
-  'Annual Retreat to be held on Friday 8th & Saturday 9th March from 5pm to 9pm & Sunday, 10th March from 8:30am to 3:30pm.',
-];
+export const announcements = [];
--- src/app/components/constants/events-data.ts
+++ src/app/components/constants/events-data.ts
@@ -3,18 +3,10 @@ import { IEvents } from '../interfaces/IEvents';
 export const eventsData: IEvents[] = [
   {
     id: 1,
-    date: new Date('2024-03-17'),
+    date: new Date('2024-03-31'),
     organised_by: 'Parish',
-    title: "St Joseph's Feast",
+    title: 'Easter',
     content:
-      "St Joseph's Feast on 17th Match. Ootunercha would be organised as part of St. Joseph's Day.",
-  },
-  {
-    id: 2,
-    date: new Date('2024-03-08'),
-    organised_by: 'Parish',
-    title: 'Elders Day',
-    content:
-      'Elders Day celebration would be organised on, 16th March 2024 from 9:00am to 12:30pm followed by lunch',
+      'In commemorating the Resurrection of Jesus, Easter also celebrates the defeat of death and the hope of salvation.',
   },
 ];
